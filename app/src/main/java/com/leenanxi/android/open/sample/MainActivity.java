6fd9971f2f2bbefc388d18b6284091beaa63af09
李南希 <linx@tiup.us>
2016-04-18 16:44:27 +0800

    update

---
+++ app/src/main/java/com/leenanxi/android/open/sample/MainActivity.java
@@ -0,0 +1,9 @@
+package com.leenanxi.android.open.sample;
+
+import com.leenanxi.android.open.qrcode.CaptureActivity;
+
+public class MainActivity extends CaptureActivity {
+
+
+
+}
---
+++ lib/src/main/java/com/google/zxing/constants/Constants.java
@@ -0,0 +1,15 @@
+package com.google.zxing.constants;
+
+/**
+ * Created by leenanxi on 16/4/18.
+ */
+public class Constants {
+    public static final int ID_START = 0X100;
+    public static final int ID_RESTART_PREVIEW = ID_START + 10;
+    public static final int ID_DECODE = ID_START + 20;
+    public static final int ID_DECODE_FAILED = ID_START + 30;
+    public static final int ID_DECODE_SUCCEEDED = ID_START + 40;
+    public static final int ID_QUIT = ID_START + 50;
+    public static final int ID_RETURN_SCAN_RESULT = ID_START + 60;
+    public static final int ID_AUTO_FOCUS = ID_START + 70;
+}
